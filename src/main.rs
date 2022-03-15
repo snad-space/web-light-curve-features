02a948867ed6385ec0c0964dd061d857fa8894a7
Konstantin Malanchev <hombit@gmail.com>
2022-03-15 16:33:33 -0500

    /versions

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,29 @@ mod v0_4_1;
 #[macro_use]
 extern crate rocket;
 use rocket::response::Redirect;
+use rocket::serde::json::Json;
 
 #[get("/help")]
 fn help() -> Redirect {
     Redirect::to("https://github.com/hombit/web-light-curve-features")
 }
 
+#[get("/versions")]
+fn versions() -> Json<&'static [&'static str]> {
+    Json(&[
+        "v0.1.17", "v0.1", "v0.2.2", "v0.2", "v0.4.1", "v0.4", "latest",
+    ])
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![v0_1_17::index, help])
+        .mount("/", routes![v0_1_17::index, help, versions])
         .mount("/api/v0.1.17/", routes![v0_1_17::index])
         .mount("/api/v0.1/", routes![v0_1_17::index])
         .mount("/api/v0.2.2/", routes![v0_2_2::index])
         .mount("/api/v0.2/", routes![v0_2_2::index])
         .mount("/api/v0.4.1/", routes![v0_4_1::index])
         .mount("/api/v0.4/", routes![v0_4_1::index])
+        .mount("/api/latest/", routes![v0_4_1::index])
 }
